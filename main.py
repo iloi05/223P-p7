bbba9535aacab5a4eccfe5fafa7daf21b02808e3
root <root@DESKTOP-8969P1M.localdomain>
2025-10-30 10:31:25 -0700

    Completed what is in instructions, need to add error checks

---
+++ main.py
@@ -0,0 +1,43 @@
+from people import Faculty
+from people import Student
+
+faculty = []
+students = []
+choices = ["1", "2", "3", "4", "9"]
+
+while True:
+    print()
+    print("     *** TUFFY TITAN STUDENT/FACULTY MAIN MENU")
+    print()
+    print("1. Add faculty")
+    print("2. Print faculty")
+    print("3. Add student")
+    print("4. Print student")
+    print("9. Exit the program")
+    print()
+
+    prompt = input("Enter menu choice: ")
+    if prompt == "1":
+        first = input("Enter first name: ")
+        last = input("Enter last name: ")
+        d = input("Enter department: ")
+        nfaculty = Faculty(first, last, d)
+        faculty.append(nfaculty)
+    elif prompt == "2":
+        Faculty.printF(faculty)
+    elif prompt == "3":
+        sfirst = input("Enter first name: ")
+        slast = input("Enter last name: ")
+        classy = input("Enter class year: ")
+        m = input("Enter major: ")
+        fa = input("Enter faculty advisor: ")
+        nstudent = Student(sfirst, slast, classy, m, fa)
+        students.append(nstudent)
+    elif prompt == "4":
+        Student.printS(students)
+    elif prompt == "9":
+        break
+    if prompt not in choices:
+        print("You've entered an invalid option, try again")
+        continue
+    
---
+++ people.py
@@ -0,0 +1,44 @@
+class Person:
+
+    def __init__(self, first_name, last_name):
+        self.firstname = first_name
+        self.lastname = last_name
+
+class Faculty(Person):
+
+    def __init__(self, first_name, last_name, department):
+        super().__init__(first_name, last_name)
+        self.department = department
+    
+    def printF(faculty_list):
+        print("======================= FACLUTY =======================")
+        print("Record  Name                  Department")
+        print("======  ====================  =========================")
+        for i, f in enumerate(faculty_list):
+            name = f"{f.firstname} {f.lastname}"
+            print(f"{i:<6} {name:<21} {f.deparment}")
+
+class Student(Person):
+    
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+    
+    def set_class(self, class_year):
+        self.classyear = class_year
+
+    def set_major(self, major):
+        self.major = major
+    
+    def set_advisor(self, advisor):
+        self.advisor = advisor
+
+    def printS(student_list):
+        print("===================================== STUDENTS ======================================")
+        print("Name                  Class      Major                       Advisor")
+        print("====================  =========  ==========================  ========================")
+        for s in student_list:
+            name = f"{s.firstname} {s.lastname}"
+            classyear = getattr(s, "classyear", "")
+            major = getattr(s, "major", "")
+            advisor = f"{s.advisor.firstname} {s.advisor.lastname}" if hasattr(s, "advisor") else ""
+            print(f"{name:<22}, {classyear:<10} {major:<25} {advisor}")
