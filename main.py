29c3aa05c44d9c57f1e4f0c44aad263e3b66fa8c
root <root@DESKTOP-8969P1M.localdomain>
2025-11-02 17:02:51 -0800

    all error checks done now, code finished

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+# Name: Ivy Loi
+# Date: 11/2/25
+# Purpose of file: Implement class functions written in people.py file
+
 from people import Faculty
 from people import Student
 
@@ -20,11 +24,20 @@ while True:
     prompt = input("Enter menu choice: ")
     if prompt == "1":
         first = input("Enter first name: ")
+        if not first.strip():
+            print("First name can not be blank, returning to main menu...")
+            continue
         last = input("Enter last name: ")
+        if not last.strip():
+            print("Last name cannot be blank, returning to main menu...")
+            continue
         d = input("Enter department: ")
+        if not d.strip():
+            print("Department cannot be left blank... returning to main menu.")
+            continue
         if not Faculty.valid_dep_len(d):
-             print("Department input must be longer than one character, returning to main menu...")
-             continue
+            print("Department input must be longer than one character, returning to main menu...")
+            continue
         if not Faculty.valid_dep(d):
             print("Invalid department entered\nSpecial characters and numbers not excepted except for spaces and hyphens(-)\nReturning to main menu...")
             continue
@@ -34,28 +47,40 @@ while True:
             Faculty.printF(faculty)
     elif prompt == "3":
         if not faculty:
-             print("You need to add at least one faculty member before adding a student.")
-             continue
+            print("You need to add at least one faculty member before adding a student.")
+            continue
         sfirst = input("Enter first name: ")
+        if not sfirst.strip():
+            print("First name can not be blank, returning to main menu...")
+            continue
         slast = input("Enter last name: ")
+        if not slast.strip():
+            print("Last name can not be blank, returning to main menu...")
+            continue
         classy = input("Enter class year (Freshman, Sophmore, Junior, Senior): ")
         if classy not in years:
              print("You've entered an invalid class year, returning to main menu...")
              continue
         m = input("Enter major: ")
+        if not m.strip():
+            print("Major cannot be left blank... returning to main menu.")
+            continue
         if not Student.valid_major_len(m):
-             print("Major input must be longer than one character, returning to main menu...")
-             continue
+            print("Major input must be longer than one character, returning to main menu...")
+            continue
         if not Student.valid_major(m):
             print("Invalid major entered\nSpecial characters and numbers not excepted except for spaces and hyphens(-)\nReturning to main menu...")
             continue
         fa = input("Enter faculty advisor (Index number of advisor from faculty advisor list): ")
         try:
-             fa = int(fa)
-             advisor = faculty[fa]
+            fa = int(fa)
+            advisor = faculty[fa]
         except (ValueError, IndexError):
-             print("Invalid advisor record number, returning to main menu...")
-             continue
+            print("Invalid advisor record number, returning to main menu...")
+            continue
+        if not Student.advisor_match(m, advisor):
+            print(f"Advisor department '{advisor.department}' does not match student major '{m}'. Returning to main menu...")
+            continue
         nstudent = Student(sfirst, slast)
         nstudent.set_class(classy)
         nstudent.set_major(m)
--- people.py
+++ people.py
@@ -1,24 +1,37 @@
-import re
+# Name: Ivy Loi
+# Date: 11/2/25
+# Purpose of file: Create classes and functions to be used in main.py file
+
+import re # To be used for valid_major and valid_dep function (error check function)
 
 class Person:
 
     def __init__(self, first_name, last_name):
+        '''Setting member variables firstname and lastname'''
         self.firstname = first_name
         self.lastname = last_name
 
 class Faculty(Person):
 
     def __init__(self, first_name, last_name, department):
+        '''Calls Person __init__ function and sets member variable department'''
         super().__init__(first_name, last_name)
         self.department = department
 
     def valid_dep(department):
+        '''Makes sure department name doesn't contain special characters'''
+        # fullmatch makes sure if input matches the accepted letters and symbols
+        # it accepts capital (A-Z) and lowercase (a-z) letters including space and hyphen
+        # returns True when accepted letters and symbols matches, false otherwise
+        # This is the same for valid_major
         return re.fullmatch(r"[A-Za-z -]+", department) is not None
     
     def valid_dep_len(department):
+        '''Makes sure department name isn't only one character long'''
         return len(department.strip()) > 1
     
     def printF(faculty_list):
+        '''Prints faculty list'''
         if not faculty_list:
             print("The list is currently empty, add some names in first")
             return "error"
@@ -33,26 +46,35 @@ class Faculty(Person):
 class Student(Person):
     
     def __init__(self, first_name, last_name):
+        '''Calls Person __init__ function'''
         super().__init__(first_name, last_name)
     
     def set_class(self, class_year):
+        '''Sets member variable classyear'''
         self.classyear = class_year
 
     def set_major(self, major):
-            self.major = major
+        '''Sets member variable major'''
+        self.major = major
     
     def set_advisor(self, advisor):
-        if isinstance(advisor, Faculty):
-            self.advisor = advisor
-            return True
+        ''' Sets member variable advisor if exists in faculty list'''
+        self.advisor = advisor
         
     def valid_major(major):
+        '''Makes sure major name does not contain special characters'''
         return re.fullmatch(r"[A-Za-z -]+", major) is not None
     
     def valid_major_len(major):
+        '''Makes sure major name is longer than one character'''
         return len(major.strip()) > 1
+    
+    def advisor_match(major, advisor):
+        '''Makes sure major matches advisor department'''
+        return major.strip().lower() == advisor.department.strip().lower()
 
     def printS(student_list):
+        '''Prints student list'''
         if not student_list:
             print("The list is currently empty, add some names first")
             return "error"
